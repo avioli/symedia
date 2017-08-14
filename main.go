8bca9939235aa8246ded8b9d40e5fdcc2c3511f0
Evo Stamatov <aviolito@gmail.com>
2017-08-14 23:24:56 +1000

    consolidate `json.go`

--- main.go
+++ main.go
@@ -618,3 +618,28 @@ func ReadVideo(fpath string) (meta FileMeta, err error) {
 	meta.Time = tm
 	return
 }
+
+func writeJSON(files FilesList, jsonFile string) error {
+	jsonBytes, err := json.MarshalIndent(files, "", "\t")
+	if err != nil {
+		return fmt.Errorf("Cannot convert files data to json.\n%s", err.Error())
+	}
+	//os.Stdout.Write(jsonBytes)
+
+	if err = ioutil.WriteFile(jsonFile, jsonBytes, 0644); err != nil {
+		return fmt.Errorf("Cannot write json: %s\n%s", jsonFile, err.Error())
+	}
+	return nil
+}
+
+func readJSON(jsonFile string) (files FilesList, err error) {
+	inFile, err := os.Open(jsonFile)
+	if err != nil {
+		return
+	}
+
+	jsonBytes := json.NewDecoder(inFile)
+	err = jsonBytes.Decode(&files)
+
+	return
+}
