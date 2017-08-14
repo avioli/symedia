ac34c8ad6e7dac31a08e3756e1a5f3e4d3dd2928
Evo Stamatov <aviolito@gmail.com>
2017-08-14 23:32:10 +1000

    consolidate `util.go`

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -673,4 +674,15 @@ func readJSON(jsonFile string) (files FilesList, err error) {
 	err = jsonBytes.Decode(&files)
 
 	return
+}
+
+// ConstructPath returns a path in the form of `YYYY/01-Jan/YYYY-MM-DD`
+func ConstructPath(tm time.Time) string {
+	return path.Join(strconv.Itoa(tm.Year()), tm.Format("01-Jan"), tm.Format("2006-01-02"))
+}
+
+// ConstructFilename returns the hard-link's filename
+func ConstructFilename(tm time.Time) string {
+	// Format: Mon Jan 2 15:04:05.000 -0700 MST 2006 or 03:04:05.000 am
+	return tm.Format("2006-01-02 15.04.05 -0700")
 }
