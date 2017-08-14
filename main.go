f1efdb5affed585bd4a4ec89f1fde4f95c6185a3
Evo Stamatov <aviolito@gmail.com>
2017-08-14 23:17:16 +1000

    consolidate `readVideo.go`

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/docopt/docopt-go"
@@ -12,13 +13,16 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
+	"time"
 )
 
 //go:generate go run scripts/embed-template.go
@@ -458,7 +462,7 @@ func init() {
 	registerCommand("process", "Process a directory for images and videos, while hard-linking them to an output directory.", cmdProcess)
 }
 
-// ReadImage reads file's metadata
+// ReadImage reads image file's metadata
 func ReadImage(fpath string) (meta FileMeta, err error) {
 	f, err := os.Open(fpath)
 	if err != nil {
@@ -498,6 +502,78 @@ func ReadImage(fpath string) (meta FileMeta, err error) {
 	meta.Height = height
 
 	tm, _err := x.DateTime()
+	if _err != nil || tm.IsZero() {
+		err = ErrSkipFile
+		return
+	}
+
+	meta.Time = tm
+	return
+}
+
+// ReadVideo reads video file's metadata
+func ReadVideo(fpath string) (meta FileMeta, err error) {
+	var cmdOut []byte
+
+	cmdName := "ffprobe"
+	// -show_format        show format/container info
+	// -show_streams       show streams info
+	cmdArgs := []string{fpath, "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams"}
+
+	// @see: https://nathanleclaire.com/blog/2014/12/29/shelled-out-commands-in-golang/
+	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		return
+	}
+
+	jsonStream := string(cmdOut)
+
+	// {
+	// 	"streams": [ {
+	// 		"tags": { "creation_time": "2016-07-18T02:29:36.000000Z" }
+	// 	}, ... ],
+	// 	"format": {
+	// 		"tags": {
+	// 			"creation_time": "2016-07-18T02:29:36.000000Z",
+	// 			"com.apple.quicktime.creationdate": "2016-07-18T12:29:35+1000"
+	// 		}
+	// 	}
+	// }
+
+	dec := json.NewDecoder(strings.NewReader(jsonStream))
+
+	var probe Ffprobe
+	if err = dec.Decode(&probe); err != nil && err != io.EOF {
+		return
+	}
+
+	for _, stream := range probe.Streams {
+		if stream.Width != 0 && stream.Height != 0 && stream.CodecType == "video" {
+			meta.Width = stream.Width
+			meta.Height = stream.Height
+			break
+		}
+	}
+
+	// check QtCreationDate first
+	tm, _err := time.Parse(tzDateLayout, probe.Format.Tags.QtCreationDate)
+	if _err != nil {
+		// check Format's CreationTime second
+		tm, _err = time.Parse(localDateLayout, probe.Format.Tags.CreationTime)
+		if _err != nil {
+			// then iterate over the Streams' CreationTimes
+			for _, stream := range probe.Streams {
+				tm, _err = time.Parse(localDateLayout, stream.Tags.CreationTime)
+				if _err == nil {
+					break
+				}
+			}
+		}
+
+		if _err == nil {
+			tm = tm.In(time.Local)
+		}
+	}
+
 	if _err != nil || tm.IsZero() {
 		err = ErrSkipFile
 		return
